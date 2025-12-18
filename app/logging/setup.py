153e5ea8c06bc64549e8c17a3fdd41d9f8ecc96b
tylerfordML <35679533+tylerfordML@users.noreply.github.com>
2025-12-18 10:31:26 -0700

    Delete app/logging directory

--- app/logging/setup.py
+++
@@ -1,7 +0,0 @@
-def setup_logging():
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(JsonFormatter())
-
-    root = logging.getLogger()
-    root.setLevel(logging.INFO)
-    root.handlers = [handler]
